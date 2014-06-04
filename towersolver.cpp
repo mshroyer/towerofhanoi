3bdc87590a26ace264ceaf0a00a3142aa67dc65c
Mark Shroyer <mshroyer@awaredigital.com>
2014-06-04 18:28:11 -0400

    Use voluntary thread interruption
    This is probably safer than using QThread::terminate, in case we manage to
    stop the thread while it's holding some mutex related to queued dispatch.
    This changeset's implementation uses multiple tests of
    isInterruptionRequested() and does not provide a way for the UI thread to
    interrupt QThread::msleep().

--- towersolver.cpp
+++ towersolver.cpp
@@ -24,20 +24,30 @@ void TowerSolver::moveTower(int n, TowerStack from, TowerStack to, TowerStack sp
      * https://en.wikipedia.org/wiki/Tower_of_Hanoi#Recursive_solution
      */
 
+    if (isInterruptionRequested())
+        return;
+
     emit moveTowerCalled(n, from, to, spare, recursion, __builtin_frame_address(0));
 
     // First, move all except bottom disk to the spare stack
     if (n > 1) {
         moveTower(n-1, from, spare, to, MoveTowerRecursion::LEFT);
+        if (isInterruptionRequested())
+            return;
     }
 
     // Second, move the bottom disk to the target stack
     emit moveDisk(from, to);
     QThread::msleep(350);
 
+    if (isInterruptionRequested())
+        return;
+
     // Third, move all except bottom disk from spare to the target stack
     if (n > 1) {
         moveTower(n-1, spare, to, from, MoveTowerRecursion::RIGHT);
+        if (isInterruptionRequested())
+            return;
     }
 
     emit moveTowerReturned();
