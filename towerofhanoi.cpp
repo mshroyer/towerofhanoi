ee375fa9c766cf0b8cd3ff85236225276dfc1727
Mark Shroyer <code@markshroyer.com>
2014-06-04 23:07:37 -0400

    Bump version and date

--- towerofhanoi.cpp
+++ towerofhanoi.cpp
@@ -42,7 +42,7 @@ void TowerOfHanoi::about()
         "<a href='https://bitbucket.org/markshroyer/towerofhanoi/'>https://bitbucket.org/markshroyer/towerofhanoi/</a></p>"
         "<p>This build was created from revision <a href='%1/commits/%3'>%4</a></p>"
         "<p>Mark Shroyer<br>"
-        "3 June 2014</p>";
+        "4 June 2014</p>";
 
     QString shortRevId { REV_ID };
     shortRevId.truncate(12);
