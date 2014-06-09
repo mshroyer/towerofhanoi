270fcc69bdfb7ab74a7a72e58efedda358b9b89c
Mark Shroyer <code@markshroyer.com>
2014-06-08 22:58:29 -0400

    Add Mac OS X deploy shell script

---
+++ materials/macdeploy.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+# Mac OS X deployment script
+
+set -e
+
+if [ $# -ne 1 ]; then
+    echo "Usage: macdeploy.sh <towerofhanoi.app>"
+    exit 1
+fi
+
+APP_BUNDLE="$1"
+PROJECT_FILE="$(dirname $0)/../towerofhanoi.pro"
+VERSION=$(awk '{ if ($1 == "ABOUT_VERSION") print $3; }' <"$PROJECT_FILE")
+DMG_FILE="towerofhanoi-mac-$VERSION.dmg"
+
+if [ -e "towerofhanoi" ]; then
+    echo "towerofhanoi folder already exists!"
+    exit 1
+fi
+
+if [ -e "$DMG_FILE" ]; then
+    echo "$DMG_FILE already exists!"
+    exit 1
+fi
+
+mkdir towerofhanoi
+cp -pr "$APP_BUNDLE" "towerofhanoi/Tower of Hanoi.app"
+macdeployqt "towerofhanoi/Tower of Hanoi.app"
+cp "$(dirname $0)/../COPYING.txt" towerofhanoi/
+cp "$(dirname $0)/../README.txt" towerofhanoi/
+
+hdiutil create -volname "Tower of Hanoi $VERSION" \
+    -srcfolder towerofhanoi -ov -format UDBZ "$DMG_FILE"
+
+rm -rf towerofhanoi
